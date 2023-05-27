2c31231c60a434fe5f272f2408ad55d569de254a
Anna Bazhenova <shchapinaa@gmail.com>
2023-05-27 22:44:05 +0200

    add tab switch on scroll

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,7 +1,7 @@
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 import BurgerIngredient from "../burger-ingredient/burger-ingredient";
 import styles from "./burger-ingredients.module.css";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { ingredientPropType } from "../../utils/prop-types";
 import PropTypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
@@ -32,33 +32,76 @@ function BurgerIngredients() {
     closeModal();
   }
 
+  const sectionRef = useRef()
+  const bunsRef = useRef()
+  const saucesRef = useRef()
+  const mainsRef = useRef()
+  
+  const handleScroll = () => {
+    const sectionRect = sectionRef.current.getBoundingClientRect();
+    const bunsRect = bunsRef.current.getBoundingClientRect();
+    const saucesRect = saucesRef.current.getBoundingClientRect();
+    const mainsRect = mainsRef.current.getBoundingClientRect();
+
+    const bunsDistance = Math.min(
+      Math.abs(bunsRect.bottom - sectionRect.top),
+      Math.abs(bunsRect.top - sectionRect.top)
+    );
+
+    const saucesDistance = Math.min(
+      Math.abs(saucesRect.bottom - sectionRect.top),
+      Math.abs(saucesRect.top - sectionRect.top)
+    );
+
+    const mainsDistance = Math.min(
+      Math.abs(mainsRect.bottom - sectionRect.top),
+      Math.abs(mainsRect.top - sectionRect.top)
+    );
+
+    let tabToSelect;
+    if (bunsDistance < saucesDistance && bunsDistance < mainsDistance) {
+      tabToSelect = "bun"
+    }
+    if (saucesDistance < bunsDistance && saucesDistance < mainsDistance) {
+      tabToSelect = "sauce"
+    }
+    if (mainsDistance < saucesDistance && mainsDistance < bunsDistance) {
+      tabToSelect = "main"
+    }
+
+    if (tab !== tabToSelect) {
+      setTab(tabToSelect)
+    }
+
+  }
+
   return (
-    <section>
+    <section ref={sectionRef}>
       <div className={styles.tab_block}>
-        <Tab value="bun" active={tab === 'bun'} onClick={setTab}>
+        <Tab value="bun" active={tab === 'bun'} onClick={() => bunsRef.current.scrollIntoView()}>
           Булки
         </Tab>
-        <Tab value="sauce" active={tab === 'sauce'} onClick={setTab}>
+        <Tab value="sauce" active={tab === 'sauce'} onClick={() => saucesRef.current.scrollIntoView()}>
           Соусы
         </Tab>
-        <Tab value="main" active={tab === 'main'} onClick={setTab}>
+        <Tab value="main" active={tab === 'main'} onClick={() => mainsRef.current.scrollIntoView()}>
           Начинки
         </Tab>
       </div>
-      <div className={`${styles.burger_ingredients} custom-scroll mt-10`}>
-        <h2 className="text text_type_main-medium">Булки</h2>
+      <div className={`${styles.burger_ingredients} custom-scroll mt-10`} onScroll={handleScroll}>
+        <h2 id="buns" ref={bunsRef} className="text text_type_main-medium">Булки</h2>
         <ul className={`${styles.list} pt-6 pl-4 pr-4`}>
           {buns.map((bun) => (
             <BurgerIngredient key={bun._id} ingredient={bun} onClick={() => showDetails(bun)}/>
           ))}
         </ul>
-        <h2 className="text text_type_main-medium mt-10">Соусы</h2>
+        <h2 id="sauces" ref={saucesRef} className="text text_type_main-medium mt-10">Соусы</h2>
         <ul className={`${styles.list} pt-6 pl-4 pr-4`}>
           {sauces.map((sauce) => (
             <BurgerIngredient key={sauce._id} ingredient={sauce} onClick={() => showDetails(sauce)}/>
           ))}
         </ul>
-        <h2 className="text text_type_main-medium mt-10">Начинки</h2>
+        <h2 id="mains" ref={mainsRef} className="text text_type_main-medium mt-10">Начинки</h2>
         <ul className={`${styles.list} pt-6 pl-4 pr-4`}>
           {mains.map((main) => (
             <BurgerIngredient key={main._id} ingredient={main} onClick={() => showDetails(main)}/>
