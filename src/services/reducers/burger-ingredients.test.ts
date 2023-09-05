e1d22ad5f2898cc66e8f718a94062d378b4d7278
Anna Bazhenova <shchapinaa@gmail.com>
2023-09-05 12:21:25 +0200

    add test fixtures

--- src/services/reducers/burger-ingredients.test.ts
+++ src/services/reducers/burger-ingredients.test.ts
@@ -5,37 +5,9 @@ import {
   GET_INGREDIENTS_SUCCESS,
 } from "../constants";
 import { burgerIngredientsReducer, initialState } from "./burger-ingredients";
+import { bun, mainIngredient, souceIngredient } from "./test-fixtures";
 
-const ingredients = [
-  {
-    _id: "60666c42cc7b410027a1a9b5",
-    name: "Говяжий метеорит (отбивная)",
-    type: "main",
-    proteins: 800,
-    fat: 800,
-    carbohydrates: 300,
-    calories: 2674,
-    price: 3000,
-    image: "https://code.s3.yandex.net/react/code/meat-04.png",
-    image_mobile: "https://code.s3.yandex.net/react/code/meat-04-mobile.png",
-    image_large: "https://code.s3.yandex.net/react/code/meat-04-large.png",
-    __v: 0,
-  },
-  {
-    _id: "60666c42cc7b410027a1a9b6",
-    name: "Биокотлета из марсианской Магнолии",
-    type: "main",
-    proteins: 420,
-    fat: 142,
-    carbohydrates: 242,
-    calories: 4242,
-    price: 424,
-    image: "https://code.s3.yandex.net/react/code/meat-01.png",
-    image_mobile: "https://code.s3.yandex.net/react/code/meat-01-mobile.png",
-    image_large: "https://code.s3.yandex.net/react/code/meat-01-large.png",
-    __v: 0,
-  },
-];
+const ingredients = [bun, mainIngredient, souceIngredient];
 
 describe("Burger ingredients reducer", () => {
   it("should return initial state", () => {
